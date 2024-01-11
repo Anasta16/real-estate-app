95ae518dd2bd4fd5404a855cbd502f515630f096
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2024-01-11 17:04:28 -0500

    completed the forgot password page functionality

--- realtor-clone-react/src/pages/ForgotPassword.jsx
+++ realtor-clone-react/src/pages/ForgotPassword.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import OAuth from '../components/OAuth';
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+import { toast } from 'react-toastify';
 
 export default function ForgotPassword() {
 
@@ -11,6 +13,20 @@ export default function ForgotPassword() {
   function onChange(e) {
     setEmail(e.target.value)
     }
+
+  async function onSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email);
+      toast.success("Email was sent!");
+      
+    } catch (error) {
+      toast.error("Could not send reset password")
+    }
+
+  }
 
   return (
     <section>
@@ -24,7 +40,7 @@ export default function ForgotPassword() {
           />
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={onSubmit}>
             <input 
               type="email"
               id="email"
