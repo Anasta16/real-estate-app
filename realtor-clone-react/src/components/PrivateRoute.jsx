6daf87eec4ff43c6d45ded909bb5ac16ae968038
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2024-01-15 23:43:31 -0500

    created spinner component

--- realtor-clone-react/src/components/PrivateRoute.jsx
+++ realtor-clone-react/src/components/PrivateRoute.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Outlet, Navigate } from 'react-router-dom';
 import { useAuthStatus } from '../hooks/useAuthStatus';
+import Spinner from './Spinner';
 
 export default function PrivateRoute() {
 
@@ -8,7 +9,7 @@ export default function PrivateRoute() {
 
     if (checkingStatus) {
         return (
-            <h3>Loading...</h3>
+          <Spinner />
         )
     }
 
