04bf8d2ebc3dbe03b19e92a4cb119d86cbe6d4fb
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2024-02-01 12:48:05 -0500

    added map to the listing page using leaflet

--- realtor-clone-react/src/pages/Listing.jsx
+++ realtor-clone-react/src/pages/Listing.jsx
@@ -10,6 +10,7 @@ import "swiper/css/bundle";
 import { FaShare, FaMapMarkerAlt, FaBed, FaBath, FaParking, FaChair } from "react-icons/fa";
 import { getAuth } from "firebase/auth";
 import Contact from '../components/Contact';
+import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
 
 export default function Listing() {
 
@@ -347,14 +348,32 @@ export default function Listing() {
             </div>
             <div
                 className="
-                    bg-blue-300
                     w-full
                     h-[200px]
-                    lg-[400px]
+                    md:h-[400px]
                     z-10
                     overflow-x-hidden
+                    mt-6
+                    md:mt-0
+                    md:ml-2
                 "
             >
+                <MapContainer 
+                    center={[listing.geolocation.lat, listing.geolocation.lng]} 
+                    zoom={13} 
+                    scrollWheelZoom={false}
+                    style={{ height: "100%", width: "100%" }}
+                >
+                    <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={[listing.geolocation.lat, listing.geolocation.lng]}>
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
 
             </div>
         </div>
